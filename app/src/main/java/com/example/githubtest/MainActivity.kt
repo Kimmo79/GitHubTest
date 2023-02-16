198fc3f0adffc5462f5dd908e8faebc8ebf2bfea
Kimmo Ropponen <105000509+Kimmo79@users.noreply.github.com>
2023-02-16 17:39:56 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/githubtest/MainActivity.kt
+++ app/src/main/java/com/example/githubtest/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.githubtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//Toimi nyt jooko
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
